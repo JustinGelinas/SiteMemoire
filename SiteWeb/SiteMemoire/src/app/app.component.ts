f86860ab09f5b09ce7464ad022303fd5d61eca49
Justin Gelinas <gelinas.justin@gmail.com>
2026-03-24 18:50:31 -0400

    feat: affiche toutes les photos dans la liste une à la suite de l'autre avec une attente de 10 secondes entre chaque et loop back quand c'est fini

--- SiteWeb/SiteMemoire/src/app/app.component.ts
+++ SiteWeb/SiteMemoire/src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Site Memoire';
 
+  timeBetweenPhotoSeconds = 10;
+
+  currentPhotoIndex = -1;
+
   readonly PhotoPathPrefix = 'assets/tempPics/';
-  currentPhotoPath = this.PhotoPathPrefix + 'WIN_20220312_10_01_49_Pro.jpg';
+
+  listOfPhotos =
+    [
+      this.PhotoPathPrefix + 'WIN_20220312_09_48_30_Pro.jpg',
+      this.PhotoPathPrefix + 'WIN_20220312_10_01_49_Pro.jpg',
+      this.PhotoPathPrefix + 'WIN_20221030_16_54_54_Pro.jpg',
+    ]
+
+  currentPhotoPath = '';
+
+  ngOnInit(): void {
+    //Fonction pour aller chercher les photos
+
+    //Affiche la première photo
+    this.displayNextPhoto();
+
+    //Démarre le timer
+    this.startTimer();
+  }
+
+  displayNextPhoto(): void {
+    this.currentPhotoIndex++;
+    if (this.currentPhotoIndex >= this.listOfPhotos.length)
+      this.currentPhotoIndex = 0;
+    this.currentPhotoPath = this.listOfPhotos[this.currentPhotoIndex];
+  }
+
+  startTimer(): void {
+    setTimeout(
+      () => {
+        this.displayNextPhoto();
+        this.startTimer();
+      },
+      this.timeBetweenPhotoSeconds * 1000
+    )
+  }
 }
