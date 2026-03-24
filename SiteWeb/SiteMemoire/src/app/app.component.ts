40d5ded80310be6875d69595e88ae82203d53e04
Justin Gelinas <gelinas.justin@gmail.com>
2026-03-24 18:41:08 -0400

    feat: montre une image sur l'écran au complet

--- SiteWeb/SiteMemoire/src/app/app.component.ts
+++ SiteWeb/SiteMemoire/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SiteMemoire';
+  title = 'Site Memoire';
+
+  readonly PhotoPathPrefix = 'assets/tempPics/';
+  currentPhotoPath = this.PhotoPathPrefix + 'WIN_20220312_10_01_49_Pro.jpg';
 }
